1d10df26245f99950354d8730d4470d4b31d698d
Aimee <aimee@parlow-co.com>
2018-08-20 17:57:35 +0200

    feat: added 'activate' Eventlistener

--- sw.js
+++ sw.js
@@ -1,55 +1,69 @@
-const staticCacheName = 'resturant-cache';
+const staticCacheName = 'resturant-cache-01';
 
-// install cache
+//install cache
 self.addEventListener('install', function(event) {
-  console.log('Install SW');
-  event.waitUntil(
-    caches.open(staticCacheName).then(function(cache) {
-      console.log('Cache all files');
-      return cache.addAll([
-        'index.html',
-        'restaurant.html',
-        'css/styles.css',
-		'css/responsive.css',
-        'js/dbhelper.js',
-        'js/main.js',
-        'js/restaurant_info.js',
-        'data/restaurants.json',
-        'img/1.jpg',
-        'img/2.jpg',
-        'img/3.jpg',
-        'img/4.jpg',
-        'img/5.jpg',
-        'img/6.jpg',
-        'img/7.jpg',
-        'img/8.jpg',
-        'img/9.jpg',
-        'img/10.jpg'
-      ]).catch(error => {
-          console.log('Caching failed');
-	  });
-    })
+	console.log('Install SW');
+	event.waitUntil(
+		caches.open(staticCacheName).then(function(cache) {
+			console.log('Cache all files');
+			return cache.addAll([
+				'index.html',
+				'restaurant.html',
+				'css/styles.css',
+				'css/responsive.css',
+				'js/dbhelper.js',
+				'js/main.js',
+				'js/restaurant_info.js',
+				'data/restaurants.json',
+				'img/1.jpg',
+				'img/2.jpg',
+				'img/3.jpg',
+				'img/4.jpg',
+				'img/5.jpg',
+				'img/6.jpg',
+				'img/7.jpg',
+				'img/8.jpg',
+				'img/9.jpg',
+				'img/10.jpg'
+			]).catch(function(error) {
+				console.log('Caching failed');
+			});
+		})
 	);
 });
 
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request).then(fetchResponse => { // new
-        return caches.open(staticCacheName).then(cache => {           // new
-          cache.put(event.request, fetchResponse.clone());            // new
-          return fetchResponse;                                       // new
-        });                                                           // new
-      });                                                             // new
-    }).catch(error => {
-      if (event.request.url.includes('.jpg')) {
-        return caches.match('/img/fixed/offline_img1.png');
-      }
-      return new Response('Not connected to the internet', {
-        status: 404,
-        statusText: "Not connected to the internet"
-      });
-    })
-  );
+
+//Fetch or return cached data
+self.addEventListener('fetch', function(fetchEvent) {
+	console.log('fetching');
+	fetchEvent.respondWith(
+		caches.match(fetchEvent.request).then(function(response) {
+			return response || fetch(fetchEvent.request).then(function(fetchResponse) {
+				return caches.open(staticCacheName).then(function(cache) {
+					cache.put(fetchEvent.request, fetchResponse.clone());
+					return fetchResponse;
+				});
+			});
+		}).catch(function(error) {
+		console.log('no internet connection!');
+		})
+	);
+})
+
+
+//delete old cache
+self.addEventListener('activate', function(activateEvent) {
+	activateEvent.waitUntil(
+		caches.keys().then(function(cacheNames) {
+			return Promise.all(
+				cacheNames.filter(function(cacheName) {
+					return cacheName.startsWith('resturant-') &&
+					cacheName !== staticCacheName;
+				}).map(function(cacheName) {
+					return caches.delete(cacheName);
+				})
+			);
+		})
+	);
 });
